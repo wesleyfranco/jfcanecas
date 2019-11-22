a189ec803c1cae34d169d794c7a8930654c4db41
Wesley Franco <wesleysilvadb@hotmail.com>
2019-11-22 14:24:32 -0200

    Colocando as quantidade de colunas

--- src/form/Input.jsx
+++ src/form/Input.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Input = ({tipo, nome, descricao, handleChange}) => (
-    <div className="col-xs-12 col-md-12 col-lg-12">
+    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <input type={tipo} name={nome} placeholder={descricao} className="form-control" onChange={handleChange} />
     </div>
 )
