044bb273e61d2b9cb6b849855f5490545aa986ea
Wesley Franco <wesleysilvadb@hotmail.com>
2019-11-22 15:07:54 -0200

    Criando as páginas do app

---
+++ src/paginas/Pedidos.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Pedidos extends Component {
+    render() {
+        return (
+            <h1>Lista de pedidos</h1>
+        )
+    }
+}
+
+export default Pedidos
+
