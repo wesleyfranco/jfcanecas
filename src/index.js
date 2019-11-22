bb78593e3148d5c6c527782f7a6b950710134acb
Wesley Franco <wesleysilvadb@hotmail.com>
2019-11-22 13:45:26 -0200

    Instalando bootstrap e iniciando o componente principal

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(, document.getElementById('root'));
+import App from './jfcanecas/App'
+
+ReactDOM.render(<App />, document.getElementById('root'));
