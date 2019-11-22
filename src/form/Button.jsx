fe84aca670d5a1971533f87fc3295d5eb3ade8a1
Wesley Franco <wesleysilvadb@hotmail.com>
2019-11-22 14:38:37 -0200

    Melhorando a formatação

--- src/form/Button.jsx
+++ src/form/Button.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Button = ({descricao, handleClick, classe}) => (
-    <div>
-        <button onClick={handleClick} className={`btn ${classe}`}>
-            {descricao}
-        </button>
+    <div className="row">
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 mt-2 text-right">
+            <button onClick={handleClick} className={`btn ${classe}`}>
+                {descricao}
+            </button>
+        </div>
     </div>
 )
 
