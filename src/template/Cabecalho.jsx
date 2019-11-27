ac80e41d0feb168d0fd6c8d2cc0d34e3261f0c43
Wesley Franco <wesleysilvadb@hotmail.com>
2019-11-27 13:45:46 -0200

    Dando espaço entre o cabeçalho e o conteudo

--- src/template/Cabecalho.jsx
+++ src/template/Cabecalho.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Menu from './Menu';
 
 const Cabecalho = ({titulo}) => (
-    <header className="page-header">
+    <header className="page-header mb-4">
         <Menu titulo={titulo} />
     </header>
 )
