b4a3516a4330eea6e4e7aa6d4c56a9bb254794a6
Wesley Franco <wesleysilvadb@hotmail.com>
2019-11-27 14:11:27 -0200

    Montando método que salva o estado

--- src/paginas/Cadastro.jsx
+++ src/paginas/Cadastro.jsx
@@ -8,13 +8,20 @@ class Cadastro extends Component {
     constructor(props) {
         super(props)
         this.state = {}
-        this.handleChange = this.handleChange.bind(this)
+        this.handleChange   = this.handleChange.bind(this)
+        this.handleClick    = this.handleClick.bind(this)
     }
     handleChange(e) {
+       // Seta o estado com os dados do formulário 
        this.setState({
            [e.target.name] : e.target.value
        })
-    }   
+    }
+    handleClick(e) {
+        e.preventDefault()
+        // Faz o cadastro
+        console.log(this.state)
+    }
     render() {
         return (
             <Form nome="cadastro">
@@ -24,7 +31,7 @@ class Cadastro extends Component {
                 <Input tipo="number" nome="qtd_itens" descricao="Quantidade de itens" handleChange={this.handleChange} />
                 <Input tipo="number" nome="valor_total" descricao="Valor total" handleChange={this.handleChange} />
                 <Input tipo="date" nome="data_entrega" descricao="Data de entrega" handleChange={this.handleChange} />
-                <Button descricao="Cadastar" classe="btn-primary" />
+                <Button descricao="Cadastar" classe="btn-primary" handleClick={this.handleClick} />
             </Form>
         )
     }
