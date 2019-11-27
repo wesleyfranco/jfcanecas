e33b4907d828c62e22e6c5bcad1b0183cb39f735
Wesley Franco <wesleysilvadb@hotmail.com>
2019-11-27 15:30:49 -0200

    Criando componente de breadcrumbs

---
+++ src/template/Breadcrumb.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Breadcrumb = ({itemAtivo}) => (
+    <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+            <li className="breadcrumb-item"> 
+                <Link to="/pedidos">Home</Link>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+               {itemAtivo}
+            </li>
+        </ol>
+    </nav>
+)
+
+export default Breadcrumb
