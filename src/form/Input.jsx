15155fe398902d4e3caf4653106123ab4a9298c7
Wesley Franco <wesleysilvadb@hotmail.com>
2019-11-27 14:19:52 -0200

    Passando o valor do input

--- src/form/Input.jsx
+++ src/form/Input.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Input = ({tipo, nome, descricao, handleChange}) => (
+const Input = ({tipo, nome, descricao, handleChange, valor}) => (
     <div className="row mb-2">
         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <input type={tipo} name={nome} placeholder={descricao} className="form-control" onChange={handleChange} />
+            <input type={tipo} name={nome} value={valor.nome} placeholder={descricao} className="form-control" onChange={handleChange} />
         </div>
     </div>
 )
