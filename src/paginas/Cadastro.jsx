4064e9da3a570107ea10007fd8df54668bacf219
Wesley Franco <wesleysilvadb@hotmail.com>
2019-11-27 13:58:06 -0200

    Trocando de class para className

--- src/paginas/Cadastro.jsx
+++ src/paginas/Cadastro.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, {Component } from 'react';
 
 import Form from '../form/Form';
 import Input from '../form/Input';
 import Button from '../form/Button';
 
-const Cadastro = (props) => (
-    <Form nome="cadastro">
-        <Input tipo="text" nome="cliente" descricao="Nome do cliente" />
-        <Input tipo="text" nome="nome_arte" descricao="Nome da arte" />
-        <Input tipo="text" nome="tipo_caneca" descricao="Tipo da caneca" />
-        <Input tipo="number" nome="qtd_itens" descricao="Quantidade de itens" />
-        <Input tipo="number" nome="valor_total" descricao="Valor total" />
-        <Input tipo="date" nome="data_entrega" descricao="Data de entrega" />
-        <Button descricao="Cadastar" classe="btn-primary" />
-    </Form>
-)
+class Cadastro extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(e) {
+       this.setState({
+           [e.target.name] : e.target.value
+       })
+       console.log(this.state)
+    }
+    render() {
+        return (
+            <Form nome="cadastro">
+                <Input tipo="text" nome="cliente" descricao="Nome do cliente" handleChange={this.handleChange} />
+                <Input tipo="text" nome="nome_arte" descricao="Nome da arte" handleChange={this.handleChange} />
+                <Input tipo="text" nome="tipo_caneca" descricao="Tipo da caneca" handleChange={this.handleChange} />
+                <Input tipo="number" nome="qtd_itens" descricao="Quantidade de itens" handleChange={this.handleChange} />
+                <Input tipo="number" nome="valor_total" descricao="Valor total" handleChange={this.handleChange} />
+                <Input tipo="date" nome="data_entrega" descricao="Data de entrega" handleChange={this.handleChange} />
+                <Button descricao="Cadastar" classe="btn-primary" />
+            </Form>
+        )
+    }
+}
 
 export default Cadastro
--- src/template/Menu.jsx
+++ src/template/Menu.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Menu = ({titulo}) => (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
             <Link to="#" className="navbar-brand">{titulo}</Link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarJFCanecas" aria-controls="navbarJFCanecas" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarJFCanecas" aria-controls="navbarJFCanecas" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarJFCanecas">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
+            <div className="collapse navbar-collapse" id="navbarJFCanecas">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
                         <Link to="/pedidos" className="nav-link">Pedidos</Link>  
                     </li>
-                    <li class="nav-item active">
+                    <li className="nav-item active">
                         <Link to="/cadastro" className="nav-link">Cadastro</Link>
                     </li>
                 </ul>
