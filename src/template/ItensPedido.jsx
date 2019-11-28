6422c1fac0fc3cac310cb8007ec432424ec38286
Wesley Franco <wesleysilvadb@hotmail.com>
2019-11-28 17:16:46 -0200

    Mostrando mensagem quando não tiver prdidos

--- src/template/ItensPedido.jsx
+++ src/template/ItensPedido.jsx
@@ -4,34 +4,43 @@ import BotaoAcao from './BotaoAcao';
 import { ConverteDataBr } from '../utils/utils';
 
 const ItensPedido = (props) => {
-    return props.lista.map((objeto) => (
-        <tr key={objeto.id}>
-            <td>
-                {objeto.cliente}
-            </td>
-            <td>
-                {objeto.nome_arte}
-            </td>
-            <td>
-                {objeto.tipo_caneca}
-            </td>
-            <td>
-                {objeto.qtd_itens}
-            </td>
-            <td>
-                {objeto.valor_total}
-            </td>
-            <td>
-                {ConverteDataBr(objeto.data_entrega)}
-            </td>
-            <td>
-                <span className="mr-2">
-                    <BotaoAcao icone="check" classe="success" handleClick={() => props.handleMarcaEntregue(objeto)} />
-                </span>
-                <BotaoAcao icone="trash-o" classe="danger" handleClick={() => props.handleExclui(objeto)} />
+    if (props.lista.length) {
+        return props.lista.map((objeto) => (
+            <tr key={objeto.id}>
+                <td>
+                    {objeto.cliente}
+                </td>
+                <td>
+                    {objeto.nome_arte}
+                </td>
+                <td>
+                    {objeto.tipo_caneca}
+                </td>
+                <td>
+                    {objeto.qtd_itens}
+                </td>
+                <td>
+                    {objeto.valor_total}
+                </td>
+                <td>
+                    {ConverteDataBr(objeto.data_entrega)}
+                </td>
+                <td>
+                    <span className="mr-2">
+                        <BotaoAcao icone="check" classe="success" handleClick={() => props.handleMarcaEntregue(objeto)} />
+                    </span>
+                    <BotaoAcao icone="trash-o" classe="danger" handleClick={() => props.handleExclui(objeto)} />
+                </td>
+            </tr>
+        ))
+    }
+    return (
+        <tr>
+            <td colSpan="7">
+                <h5 className="text-center">Nenhum pedido encontrado</h5>
             </td>
         </tr>
-    ))
+    )   
 }
 
 export default ItensPedido
