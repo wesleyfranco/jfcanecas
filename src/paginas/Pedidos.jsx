4de1ccf19198c1386725bbc7d5324f11a6bcf1e0
Wesley Franco <wesleysilvadb@hotmail.com>
2019-11-28 14:20:06 -0200

    Buscando registros via API

--- src/paginas/Pedidos.jsx
+++ src/paginas/Pedidos.jsx
@@ -1,45 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Breadcrumb from '../template/Breadcrumb';
 import ItensPedido from '../template/ItensPedido';
 
+const URL = 'http://localhost:8080/';
+
 class Pedidos extends Component {
     constructor(props) {
         super(props)
-        this.state = {lista: [
-            {
-                id: '1',
-                cliente: 'Wesley',
-                nome_arte: 'Homem aranha',
-                tipo_caneca: 'Long Drink',
-                qtd_itens: 150,
-                valor_total: 220.00,
-                data_entrega: '2019-12-10'
-            },
-            {
-                id: '2',
-                cliente: 'Paulo',
-                nome_arte: 'Esqueleto',
-                tipo_caneca: 'Long Drink',
-                qtd_itens: 80,
-                valor_total: 130.00,
-                data_entrega: '2019-12-20'
-            },
-            {
-                id: '3',
-                cliente: 'César',
-                nome_arte: 'Palhaço',
-                tipo_caneca: 'Tulipa',
-                qtd_itens: 50,
-                valor_total: 80.00,
-                data_entrega: '2019-12-02'
-            }
-        ]}
+        this.state = { lista: [] }
         this.handleMarcaEntregue = this.handleMarcaEntregue.bind(this)
         this.handleExclui = this.handleExclui.bind(this)
-    }
-    atualiza() {
 
+        this.atualiza()
+    }
+    atualiza() {        
+        axios.get(URL)
+        .then((resposta) => {
+            this.setState({ lista: resposta.data })
+        })
     }
     handleMarcaEntregue(objeto) {
         alert(objeto.id)
