4eed24ef42f40cf608b3339fdfa3eb78ce5045b2
Wesley Franco <wesleysilvadb@hotmail.com>
2019-11-28 13:55:59 -0200

    Utilizando switch case

--- src/api/pedidos/index.php
+++ src/api/pedidos/index.php
@@ -4,16 +4,21 @@ require_once 'classes/Pedidos.php';
 
 $pedidos    = new Pedidos;
 $metodoHttp = $_SERVER['REQUEST_METHOD'];
-if ($metodoHttp === 'GET') {
-    $pesquisa = (isset($_GET['cliente'])) ? $_GET['cliente'] : '';
-    $pedidos->retornaPedidos($pesquisa);
-} else if($metodoHttp === 'POST') {
-
-} else if($metodoHttp === 'PUT') {
-    $idAtualizacao      = $_SERVER['PATH_INFO'];
-    $dadosAtualizacao   = file_get_contents('php://input');
-} else if($metodoHttp === 'DELETE') {
-
-} else {
-    echo json_encode(['erro' => 'Método não aceito']);
+switch ($metodoHttp) {
+    case 'GET':
+        $pesquisa = (isset($_GET['cliente'])) ? $_GET['cliente'] : '';
+        $pedidos->retornaPedidos($pesquisa);
+        break;
+    case 'POST':
+        echo "POST";
+        break;
+    case 'PUT':
+        $idAtualizacao      = $_SERVER['PATH_INFO'];
+        $dadosAtualizacao   = file_get_contents('php://input');
+        break;
+    case 'DELETE':
+        $idAtualizacao      = $_SERVER['PATH_INFO'];
+        break; 
+    default:
+        echo json_encode(['erro' => 'Método não suportado']);      
 }
