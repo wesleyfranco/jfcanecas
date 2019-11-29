448b954f58f73daf064cf0ba27b2a5c9fe7d1f41
Wesley Franco <wesleysilvadb@hotmail.com>
2019-11-29 15:10:00 -0200

    Montando a grid da pesquisa

--- src/template/Pesquisa.jsx
+++ src/template/Pesquisa.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 import Input from '../form/Input';
 import Button from '../template/BotaoAcao';
+import { MontaGrid } from '../utils/utils';
 
 const Pesquisa = (props) => (
-    <div>
-        <Input tipo="search" nome="pesquisa_cliente" descricao="Digite o nome do cliente" handleChange={props.handleChangePesquisa} valor={props.valor} />
-        <Button icone="search" classe="info" handleClick={props.handlePesquisa} />
-        <Button icone="close" classe="danger" handleClick={props.handleLimpar} />
+    <div className="row mb-3">
+        <MontaGrid colunas="12 12 10 10" semLinha={true}>
+            <Input tipo="search" nome="pesquisa_cliente" descricao="Digite o nome do cliente" handleChange={props.handleChangePesquisa} valor={props.valor} />
+        </MontaGrid>
+        <MontaGrid colunas="12 12 2 2" semLinha={true}>
+            <Button icone="search" classe="info" handleClick={props.handlePesquisa} />
+            <Button icone="close" classe="danger" handleClick={props.handleLimpar} />
+        </MontaGrid>
     </div>
 )
 
