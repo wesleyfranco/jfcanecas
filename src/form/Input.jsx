34ad2cb4b0993aaaae73754bc21cb3e2fc93b34b
Wesley Franco <wesleysilvadb@hotmail.com>
2019-11-29 14:56:44 -0200

    Retirando o grid padrão

--- src/form/Input.jsx
+++ src/form/Input.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
-const Input = ({tipo, nome, descricao, handleChange, valor}) => (
-    <div className="row mb-2">
-        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <input type={tipo} name={nome} value={valor[nome]} placeholder={descricao} className="form-control" onChange={handleChange} />
-        </div>
-    </div>
+const Input = ({tipo, nome, descricao, handleChange, valor}) => (   
+    <input type={tipo} name={nome} value={valor[nome]} placeholder={descricao} className="form-control" onChange={handleChange} />
 )
 
 export default Input
