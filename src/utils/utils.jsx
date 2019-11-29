03d9d09e27ab63aae122e9e0eb8d69a916d77ed4
Wesley Franco <wesleysilvadb@hotmail.com>
2019-11-29 14:56:08 -0200

    Melhorando componente que gera o grid do boostrap

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -1,17 +1,24 @@
+import React from 'react';
+
 const ConverteDataBr = (dataUs) => {
     const dataArray = dataUs.split('-')
     const dataBr = dataArray[2] + '/' + dataArray[1] + '/' + dataArray[0]
     return dataBr
 }
 
-const MontaGrid = (colunas) => {
-    const numeroColunas = colunas.split(' ')
+const MontaGrid = (props) => {
+    const numeroColunas = props.colunas.split(' ')
     let classesColunas = `col-xs-${numeroColunas[0]}`
     classesColunas += ` col-sm-${numeroColunas[1]}`
     classesColunas += ` col-md-${numeroColunas[2]}`
-    classesColunas += ` col-lg-${numeroColunas[3]}`
-    return classesColunas
-    
+    classesColunas += ` col-lg-${numeroColunas[3]}`   
+    return (
+        <div className="row mb-2">
+            <div className={classesColunas}>
+                {props.children}
+            </div>
+        </div>
+    )
 }
     
 
