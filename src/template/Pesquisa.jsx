cc18d20f09eb3a77f1c194c2f9be1d076fe411d1
Wesley Franco <wesleysilvadb@hotmail.com>
2019-12-04 15:12:17 -0200

    Trocando o texto para pesquisar por nome ou telefone

--- src/template/Pesquisa.jsx
+++ src/template/Pesquisa.jsx
@@ -7,7 +7,7 @@ import { MontaGrid } from '../utils/utils';
 const Pesquisa = (props) => (
     <div className="row mb-3">
         <MontaGrid colunas="7 7 10 10" semLinha={true}>
-            <Input tipo="search" nome="pesquisa_cliente" descricao="Digite o nome do cliente" handleChange={props.handleChangePesquisa} valor={props.valor} />
+            <Input tipo="search" nome="pesquisa_cliente" descricao="Digite o nome do cliente ou telefone" handleChange={props.handleChangePesquisa} valor={props.valor} />
         </MontaGrid>
         <MontaGrid colunas="5 5 2 2" semLinha={true}>
             <span className="mr-1">
