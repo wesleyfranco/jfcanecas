9520c1a71019ab4db5b90217e99e6005adc13127
Wesley Franco <wesleysilvadb@hotmail.com>
2019-12-04 14:23:20 -0200

    Diminuindo o tamanho das colunas para telas pequenas

--- src/template/Pesquisa.jsx
+++ src/template/Pesquisa.jsx
@@ -6,11 +6,11 @@ import { MontaGrid } from '../utils/utils';
 
 const Pesquisa = (props) => (
     <div className="row mb-3">
-        <MontaGrid colunas="12 12 10 10" semLinha={true}>
+        <MontaGrid colunas="7 7 10 10" semLinha={true}>
             <Input tipo="search" nome="pesquisa_cliente" descricao="Digite o nome do cliente" handleChange={props.handleChangePesquisa} valor={props.valor} />
         </MontaGrid>
-        <MontaGrid colunas="12 12 2 2" semLinha={true}>
-            <span className="mr-2">
+        <MontaGrid colunas="5 5 2 2" semLinha={true}>
+            <span className="mr-1">
                 <Button icone="search" classe="info" handleClick={props.handlePesquisa} />
             </span>
             <Button icone="close" classe="danger" handleClick={props.handleLimpar} />
