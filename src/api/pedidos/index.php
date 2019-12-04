fd6919a24563bcc77ffcdb0d8509993a9aa85345
Wesley Franco <wesleysilvadb@hotmail.com>
2019-12-04 17:25:14 -0200

    Setando o timezone

--- src/api/pedidos/index.php
+++ src/api/pedidos/index.php
@@ -6,6 +6,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE");
 
+date_default_timezone_set('America/Sao_Paulo');
+
 $pedidos    = new Pedidos;
 $metodoHttp = $_SERVER['REQUEST_METHOD'];
 
