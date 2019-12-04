ec0af4e13403f2146873e98d65c212ef38d4dabe
Wesley Franco <wesleysilvadb@hotmail.com>
2019-12-04 15:52:59 -0200

    Ajustando colunas da pesquisa para o mobile

--- src/template/Pesquisa.jsx
+++ src/template/Pesquisa.jsx
@@ -6,10 +6,10 @@ import { MontaGrid } from '../utils/utils';
 
 const Pesquisa = (props) => (
     <div className="row mb-3">
-        <MontaGrid colunas="7 7 10 10" semLinha={true}>
+        <MontaGrid colunas="8 10 10 10" semLinha={true}>
             <Input tipo="search" nome="pesquisa_cliente" descricao="Digite o nome do cliente ou telefone" handleChange={props.handleChangePesquisa} valor={props.valor} />
         </MontaGrid>
-        <MontaGrid colunas="5 5 2 2" semLinha={true}>
+        <MontaGrid colunas="4 2 2 2" semLinha={true}>
             <span className="mr-1">
                 <Button icone="search" classe="info" handleClick={props.handlePesquisa} />
             </span>
